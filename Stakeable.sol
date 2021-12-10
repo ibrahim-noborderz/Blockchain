b1a5978d5750f2827706f45d53e752ed352f3dc4
Ibrahim Iqbal <ibrahim@noborderz.com>
2021-12-10 19:13:42 +0500

    Improved Voting smart contract, replaced previous/out-dated staking contract with the new implementation

--- Stakeable.sol
+++ Stakeable.sol
@@ -1,97 +1,233 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 
-contract Stakeable {
+/*
+    Imports for Remix
+*/
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/security/ReentrancyGuard.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol";
 
-    struct Stake {
-        address user;
-        uint256 amount;
-        uint256 since;
-        uint256 claimable;
+/*
+    Imports for local development
+*/
+import "openzeppelin-contracts/access/Ownable.sol";
+import "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/utils/ReentrancyGuard.sol";
+import "openzeppelin-contracts/utils/Address.sol";
+
+// Importing Reward Token Smart Contract
+import "./EBToken.sol";
+
+contract Stakeable is Ownable, ReentrancyGuard {
+
+    /*
+        Variable Declaration Start
+    */
+
+    // IERC20 EBToken = IERC20(0xd9145CCE52D386f254917e481eB44e9943F39138);
+    EBToken internal rewardToken;
+
+    struct RewardPriceTrack {
+        uint256 rewardPerBlock;
+        uint256 fromBlock;
+        uint256 toBlock;
     }
 
-    struct Stakeholder {
-        address user;
-        Stake[] address_stakes;
-    }
- 
-    struct StakingSummary {
-        uint256 total_amount;
-        Stake[] stakes;
-    }
+    RewardPriceTrack[] internal rewardPriceTrack;
 
-    Stakeholder[] internal stakeholders;
+    address[] internal stakeholders;
 
-    mapping (address => uint256) internal stakes;
- 
-    event Staked (address indexed user, uint256 amount, uint256 index, uint256 timestamp);
+    mapping(address => uint256) internal userStakeAmount;
 
-    uint256 internal rewardPerHour = 1000;
+    mapping(address => uint256) internal userStakeBlock;
 
-    constructor () {
-        stakeholders.push();
+    /*
+        Variable Declaration End
+    */
+
+    constructor (uint256 initialReward) {
+        rewardToken = new EBToken();
+        rewardPriceTrack.push(RewardPriceTrack(initialReward, block.number, 0));
     }
 
-    function _addStakeholder (address staker) internal returns (uint256) {
-        stakeholders.push();
-        uint256 userIndex = stakeholders.length - 1;
-        stakeholders[userIndex].user = staker;
-        stakes[staker] = userIndex;
-        return userIndex; 
+    /*
+        Events Start
+    */
+
+    event Staked (address indexed stakeholder, uint256 amount, uint onBlockNumber);
+
+    event Unstaked (address indexed stakeholder, uint256 reward, uint256 blockNumber);
+
+    event Claimed (address indexed stakeholder, uint256 reward, uint256 blockNumber);
+
+    event TransferredFromUserToContract (address indexed user, uint256 amount, uint256 blockNumber);
+
+    event TransferredFromContractToUser (address indexed user, uint256 amount, uint256 blockNumber);
+
+    event InjectedRewardTokens (address indexed by, uint256 amount, uint256 blockNumber);
+
+    event RewardPerBlockUpdated (address indexed by, uint256 rewardValue, uint256 blockNumber);
+
+    /*
+        Events End
+    */
+
+    /*
+        Modifiers Start
+    */
+
+    modifier isAllowedToStake () {
+        (bool _isStakeHolder, ) = isStakeHolder();
+        require(!_isStakeHolder, "You've currently staked some assets, please claim/withdraw to stake again.");
+        _;
     }
 
-    function _stake (uint256 _amount) internal {
-        require(_amount > 0, "Cannot stake nothing");
-        
-        uint256 index = stakes[msg.sender];
-        uint256 timestamp = block.timestamp;
-        
-        if (index == 0) {
-            index = _addStakeholder(msg.sender);
+    modifier isClaimable () {
+        (bool _isStakeHolder, ) = isStakeHolder();
+        require(_isStakeHolder, "You've nothing to claim!!!");
+        _;
+    }
+
+    /*
+        Modifiers End
+    */
+
+    /*
+        Helper Methods Start
+    */
+
+    function isContract(address account) internal view returns (bool) {
+        uint256 size;
+        assembly {
+            size := extcodesize(account)
         }
-
-        stakeholders[index].address_stakes.push(Stake(msg.sender, _amount, timestamp,0));
-        
-        // Emit an event that the stake has occured
-        emit Staked (msg.sender, _amount, index, timestamp);
-    }
-      
-    function calculateStakeReward (Stake memory _current_stake) internal view returns (uint256) {
-        return (((block.timestamp - _current_stake.since) / 1 hours) * _current_stake.amount) / rewardPerHour;
+        return size > 0;
     }
 
-    function _withdrawStake (uint256 amount, uint256 index) internal returns (uint256) {
-        uint256 user_index = stakes[msg.sender];
-        Stake memory current_stake = stakeholders[user_index].address_stakes[index];
-        
-        require(current_stake.amount >= amount, "Staking: Cannot withdraw more than you have staked");
-        
-        uint256 reward = calculateStakeReward(current_stake);
-        current_stake.amount = current_stake.amount - amount;
-        
-        if(current_stake.amount == 0){
-            delete stakeholders[user_index].address_stakes[index];
+    function isStakeHolder () internal view returns (bool, uint) {
+        uint index = 0;
+        for (uint256 i = 0; i < stakeholders.length; i++) {
+            index = i;
+            if (stakeholders[i] == msg.sender) return (true, index);
+        }
+        return (false, index);
+    }
+
+    function removeStakeHolder () internal {
+        (bool _isStakeHolder, uint256 index) = isStakeHolder();
+
+        if (_isStakeHolder) {
+            stakeholders[index] = stakeholders[stakeholders.length - 1];
+            stakeholders.pop();
+        }
+    }
+
+    function calculateUserReward (address user) internal view returns (uint256) {
+        /*
+            1) Get block number when user staked from 'userStakeBlock'
+            2) Check if latest value of 'fromBlock' is equal to current block.number;
+            // if so, latest 'rewardPerBlock' value shall apply to (block.number - userStakeBlock)
+            3) Else, get index where 'userStakeBlock' equals 'fromBlock' in 'rewardPriceTrack'
+            4) Start looping from index found in 3) reward shall be the sum of (toBlock - fromBlock) * rewardPerBlock
+            // and for index == rewardPriceTrack.length-1 value to add will be ( (block.number - fromBlock) * rewardPerBlock)
+            // if block.number - fromBlock equals zero then rewardPerBlock shall be added to total reward calculated. 
+        */
+        uint256 userBlock = userStakeBlock[user];
+        uint256 latestRewardBlock = rewardPriceTrack[rewardPriceTrack.length - 1].fromBlock;
+        if (userBlock == block.number) return rewardPriceTrack[rewardPriceTrack.length - 1].rewardPerBlock * userStakeAmount[user];
+        else if (latestRewardBlock == block.number || userBlock > latestRewardBlock) {
+            return ((block.number - userBlock) * rewardPriceTrack[rewardPriceTrack.length - 1].rewardPerBlock) * userStakeAmount[user];
         } else {
-            stakeholders[user_index].address_stakes[index].amount = current_stake.amount;
-            stakeholders[user_index].address_stakes[index].since = block.timestamp;    
+            uint256 reward = 0;
+            uint rewardStartIndex = getRewardStartIndex(userBlock);
+            for (rewardStartIndex; rewardStartIndex < rewardPriceTrack.length - 1; rewardStartIndex++) {
+                reward += ((rewardPriceTrack[rewardStartIndex].toBlock - rewardPriceTrack[rewardStartIndex].fromBlock) * rewardPriceTrack[rewardStartIndex].rewardPerBlock) * userStakeAmount[user];
+            }
+            return reward;
         }
-
-        return amount + reward;
     }
 
-    function hasStake (address _staker) public view returns (StakingSummary memory) {
-        uint256 totalStakeAmount; 
-        
-        StakingSummary memory summary = StakingSummary(0, stakeholders[stakes[_staker]].address_stakes);
-        
-        for (uint256 s = 0; s < summary.stakes.length; s += 1) {
-           uint256 availableReward = calculateStakeReward(summary.stakes[s]);
-           summary.stakes[s].claimable = availableReward;
-           totalStakeAmount = totalStakeAmount+summary.stakes[s].amount;
+    function getRewardStartIndex (uint userBlock) internal view returns (uint) {
+        for (uint i = 0; i < rewardPriceTrack.length - 2; i++) {
+            if (rewardPriceTrack[i].fromBlock <= userBlock && rewardPriceTrack[i].toBlock >= userBlock) {
+                return i;
+            }
         }
-        
-        summary.total_amount = totalStakeAmount;
-        return summary;
+        return 0;
     }
+
+    /*
+        Helper Methods End
+    */
+
+    /*
+        Admin Only Methods Start
+    */
+
+    function getLatestRewardPerBlock () public onlyOwner view returns (uint256) {
+        return rewardPriceTrack[rewardPriceTrack.length - 1].rewardPerBlock;
+    }
+
+    function getRewardHistory () public onlyOwner view returns (RewardPriceTrack[] memory) {
+        return rewardPriceTrack;
+    }
+
+    function setRewardPerBlock (uint256 rewardPerBlock) public onlyOwner {
+        rewardPriceTrack[rewardPriceTrack.length - 1].toBlock = block.number - 1;
+        rewardPriceTrack.push(RewardPriceTrack(rewardPerBlock, block.number, 0));
+        emit RewardPerBlockUpdated(msg.sender, rewardPerBlock, block.number);
+    }
+
+    function injetRewardToken (uint256 amountInEther) public onlyOwner {
+        rewardToken.mintAmountAsEther(amountInEther);
+        emit InjectedRewardTokens(msg.sender, amountInEther * (10 ** 18), block.number);
+    }
+
+    /*
+        Admin Only Methods End
+    */
+
+    /*
+        Public Methods Start
+    */
+
+    function stake (address tokenAddress, uint256 amountInWei) payable public isAllowedToStake nonReentrant {
+        require(isContract(tokenAddress), "Provided address doesn't belong to a valid contract");
+        require (amountInWei > 0, "Cannot stake nothing");
+        IERC20 userToken = IERC20(tokenAddress);
+        // Checking user balance
+        require (userToken.balanceOf(msg.sender) > amountInWei, "Insufficient balance! Stake amount exceeds current balance");
+        // Processing payment
+        require(userToken.transferFrom(msg.sender, address(this), amountInWei), "Payment failed!!! Please make sure you've approved amount you want to stake and try again.");
+        emit TransferredFromUserToContract(msg.sender, amountInWei, block.number);
+
+        userStakeAmount[msg.sender] = amountInWei;
+        userStakeBlock[msg.sender] = block.number;
+        stakeholders.push(msg.sender);
+        emit Staked(msg.sender, amountInWei, block.number);
+    }
+
+    function claimReward () payable public isClaimable nonReentrant {
+        uint256 reward = calculateUserReward(msg.sender);
+        // Minting number of reward tokens to be transferred to staker
+        rewardToken.mintAmountAsWEI(reward);
+        emit InjectedRewardTokens(msg.sender, reward, block.number);
+        // Approving reward tokens for staker
+        rewardToken.approve(msg.sender, reward);
+        // Transferring reward to staker
+        rewardToken.transfer(msg.sender, reward);
+        emit TransferredFromContractToUser(msg.sender, reward, block.number);
+        // Removing Staker's data
+        delete userStakeAmount[msg.sender];
+        delete userStakeBlock[msg.sender];
+        removeStakeHolder();
+        emit Claimed(msg.sender, reward, block.number);
+    }
+
+    /*
+        Public Methods End
+    */
 
 }
--- Voting.sol
+++ Voting.sol
@@ -1,27 +1,64 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 
-contract Voting {
+/*
+    Imports
+*/
+import "openzeppelin-contracts/utils/ReentrancyGuard.sol";
+import "openzeppelin-contracts/access/Ownable.sol";
 
+contract Voting is Ownable, ReentrancyGuard {
+
+    /*
+        Variable Declaration Start
+    */
     struct Voter {
         address _address;
-        bool isInFavor;
-        uint balance;
+        uint256 isInFavor;
+        uint256 balance;
     }
 
     Voter[] private voters;
     address private owner;
 
+    uint256 private votingStartTime = 0;
+    uint256 private votingEndTime = 0;
+    uint256 private votesInFavor = 0;
+    uint256 private votesAgainst = 0;
+    /*
+        Variable Declaration End
+    */
+
     constructor () {
         owner = msg.sender;
     }
 
+    /*
+        Events Start
+    */
+    event Voting_Started (address by, uint256 time);
+    event Voting_Ended (address by, uint256 total_votes ,uint256 time);
+    event Vote_Casted (address voter, uint256 decision, uint256 time);
+    event Voting_Reset (address by, uint256 time);
+    event Get_Voters (Voter[] voters);
+    event Get_Total_Votes (address viewer, uint256 votes, uint256 time);
+    event Get_Total_Voters (address viewer, uint256 voters, uint256 time);
+    event Get_Votes_In_Favor (uint256 votes_in_favor);
+    event Get_Votes_Against (uint256 votes_against);
+    /*
+        Events End
+    */
+
+    /*
+        Modifiers Start
+    */
     modifier validateVoter () {
+        // Prevent owner from voting himself 
         require (msg.sender != owner, "Owner cannot cast votes!");
         
         bool isVoterValid = true;
 
-        for (uint i = 0; i < voters.length; i++) {
+        for (uint256 i = 0; i < voters.length; i++) {
             if (voters[i]._address == msg.sender) {
                 isVoterValid = false;
                 break;
@@ -32,37 +69,111 @@ contract Voting {
         _;
     }
 
-    function VoteInFavor () public validateVoter {
-        voters.push( Voter(msg.sender, true, (msg.sender).balance) );
+    modifier votingStarted () {
+        require (votingStartTime != 0, "Voting has not started yet!");
+        _;
     }
 
-    function VoteAgainst () public validateVoter {
-        voters.push( Voter(msg.sender, false, (msg.sender).balance) );
+    modifier votingEnded () {
+        require(votingEndTime != 0 && votingStartTime <= block.timestamp 
+        && block.timestamp >= votingEndTime, "Voting has not ended yet!");
+        _;
     }
 
-    function TotalVotesCasted () public view returns (uint totalVotes){
-        totalVotes = voters.length;
+    modifier validateVoting () {
+        require (block.timestamp <= votingEndTime, "Voting has ended!");
+        _;
     }
+    /*
+        Modifiers End
+    */
 
-    function GetResult () public view returns (string memory) {
-        uint coinsInFavor = 0;
-        uint coinsAgainst = 0;
-        
-        for (uint i = 0; i < voters.length; i++) {
-            if (voters[i].isInFavor) {
-                coinsInFavor += voters[i].balance;
+    /*
+        Admin methods start
+    */
+
+    function calculateResults () private {
+        for (uint256 i = 0; i < voters.length; i++) {
+            if (voters[i].isInFavor == 1) {
+                votesInFavor += voters[i].balance;
             } else {
-                coinsAgainst += voters[i].balance;
+                votesAgainst += voters[i].balance;
             }
         }
+    }
 
-        if (coinsInFavor > coinsAgainst) {
+    function startVoting () public onlyOwner {
+        require(votingStartTime == 0, "Voting has already started");
+        votingStartTime = block.timestamp;
+        emit Voting_Started(msg.sender, block.timestamp);
+    }
+
+    function endVoting () public onlyOwner votingStarted {
+        votingEndTime = block.timestamp;
+        calculateResults();
+        emit Voting_Ended(msg.sender, votesInFavor + votesAgainst, block.timestamp);
+    }
+
+    function reset () public onlyOwner votingEnded {
+        votingStartTime = 0;
+        votingEndTime = 0;
+        delete voters;
+        emit Voting_Reset(msg.sender, block.timestamp);
+    }
+
+    function ShowVotersWithResults () public view onlyOwner votingEnded returns (Voter[] memory) {
+        emit Get_Voters(voters);
+        return voters;
+    }
+    /*
+        Admin methods end
+    */
+
+    /*
+        User/Public methods start
+    */
+
+    function Vote (uint256 _vote) public votingStarted validateVoter validateVoting {
+        // Validating input
+        require (_vote >= 0 && _vote <= 1,
+        "Please enter either 0 for No or 1 for Yes ");
+        voters.push(Voter(msg.sender, _vote, (msg.sender).balance));
+        emit Vote_Casted(msg.sender, _vote, block.timestamp);
+    }
+
+    function TotalVoters () public view votingEnded returns (uint256) {
+        emit Get_Total_Voters(msg.sender, voters.length, block.timestamp);
+        return voters.length;
+    }
+
+    function TotalVotes () public view votingEnded returns (uint256) {
+        uint256 totalVotes = votesInFavor + votesAgainst;
+        emit Get_Total_Votes(msg.sender, totalVotes, block.timestamp);
+        return totalVotes;
+    }
+
+    function VotesInFavor () public view votingEnded returns (uint256) {
+        emit Get_Votes_In_Favor(votesInFavor);
+        return votesInFavor;
+    }
+
+    function VotesAgainst () public view votingEnded returns (uint256) {
+        emit Get_Votes_Against(votesAgainst);
+        return votesAgainst;
+    }
+
+    function GetVotingResults () public view returns (string memory) {
+        if (votesInFavor > votesAgainst) {
             return "Majority is in Favor, Candidate WON!!!";
-        } else if (coinsInFavor < coinsAgainst) {
+        } else if (votesInFavor < votesAgainst) {
             return "Majority is in Favor, Candidate LOST!!!";
         } else {
             return "Equal worth of votes on both sides, It's a DRAW!!!";
         }
     }
+
+    /*
+        User/Public methods end
+    */
 
 }
